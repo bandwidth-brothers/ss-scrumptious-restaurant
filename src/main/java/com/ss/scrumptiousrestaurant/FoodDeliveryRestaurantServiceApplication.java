53ca2d2bd99a025fec77ab858bc398b1213927f5
brunoara21 <bruno.rebaza@smoothstack.com>
2021-08-26 16:19:18 -0400

    Initial Spring commit

---
+++ src/main/java/com/ss/scrumptiousrestaurant/FoodDeliveryRestaurantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ss.scrumptiousrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryRestaurantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryRestaurantServiceApplication.class, args);
+	}
+
+}
