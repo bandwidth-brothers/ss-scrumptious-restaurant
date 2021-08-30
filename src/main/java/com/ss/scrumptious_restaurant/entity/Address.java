23ff62c48b19da80d30b40aeb20480d5e4ce86f9
brunoara21 <bruno.rebaza@smoothstack.com>
2021-08-30 02:44:50 -0400

    BB-69 feat: Manually Create Restaurant as an Admin with Unit Tests

---
+++ src/main/java/com/ss/scrumptious_restaurant/entity/Address.java
@@ -0,0 +1,55 @@
+package com.ss.scrumptious_restaurant.entity;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.lang.Nullable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="ADDRESS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "BINARY(16)", name = "addressId", updatable = false)
+    private UUID addressId;
+	
+	@OneToOne(mappedBy = "address")
+    @EqualsAndHashCode.Exclude
+	private Restaurant restaurant;
+	
+	@NotBlank
+    @Column(name="lineOne")
+	private String lineOne;
+	
+	@Nullable
+	@Column(name="lineTwo")
+	private String lineTwo;
+	
+	@NotBlank
+	private String city;
+	
+	@NotBlank
+	private String state;
+	
+	@NotBlank
+	private String zip;
+}
---
+++ src/main/java/com/ss/scrumptious_restaurant/entity/PriceCategory.java
@@ -0,0 +1,30 @@
+package com.ss.scrumptious_restaurant.entity;
+
+public enum PriceCategory {
+	$(1),
+	$$(2),
+	$$$(3);
+	
+	private final Integer priceCategory;
+	
+	private PriceCategory(Integer priceCategory) {
+		this.priceCategory = priceCategory;
+	}
+	
+	public Integer getPriceLevel() {
+		return priceCategory;
+	}
+	
+	public String getPriceCategory() {
+		switch(priceCategory) {
+		case 1:
+			return "$";
+		case 2:
+			return "$$";	
+		case 3:
+			return "$$$";
+		default:
+			return "$";
+		}
+	}
+}
